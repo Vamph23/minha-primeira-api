2fd1f67cf9e8b1f100ad6ad9901e4cf57c18e61b
MARIA EDUARDA PEREIRA DE SOUSA <156702508+Vamph23@users.noreply.github.com>
2024-01-30 17:35:59 -0300

    Revisão da aula

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import express from "express"
+import express from 'express';
+import {db,firestore} from '../banco_de_dados/firebase';
 
 const app = express();
 
 
 
 app.listen(300,function (){
-    console.log("serviço rodando em http://locelhost:3000");
+    console.log("serviço rodando na porta em http://locelhost:3000");
 });

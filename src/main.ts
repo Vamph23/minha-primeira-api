23db86528b4f3710a12962966d09b753c82ea2db
MARIA EDUARDA PEREIRA DE SOUSA <156702508+Vamph23@users.noreply.github.com>
2024-01-31 17:43:42 -0300

    criação de rota

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
 import express from 'express';
 import {db,firestore} from '../banco_de_dados/firebase';
+import { doc } from 'firebase/firestore';
 
 const app = express();
 
+app.get('/',(req,res) =>{   
+    res.send("Sejam bem vindos à minha primeira API");
+});
 
+app.post('/usuario', async (req, res) =>{
+    const usuario  = req.body.nome
+    try {
+        const docRef = await firestore.addDoc(firestore.collection(db, 'usuario'),{
+            nome: nome,
+        })
+        res.send(docRef.id)
+    } catch (e) {
+        console.log(e) 
 
-app.listen(300,function (){
-    console.log("serviço rodando na porta em http://locelhost:3000");
+        res.status(500).send(e)
+
+        
+    }
+})
+
+app.listen(3000,function (){
+    console.log("serviço rodando na porta em http://localhost:3000");
 });

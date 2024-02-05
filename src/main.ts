6f48915eed4f4c917c9418afec609590f9bd047f
MARIA EDUARDA PEREIRA DE SOUSA <156702508+Vamph23@users.noreply.github.com>
2024-02-05 16:38:08 -0300

    Ùltima aula

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,29 @@
 import express from 'express';
 import {db,firestore} from '../banco_de_dados/firebase';
 import { doc } from 'firebase/firestore';
+import { json } from 'stream/consumers';
 
 const app = express();
+
+app.use(express.json())
 
 app.get('/',(req,res) =>{   
     res.send("Sejam bem vindos à minha primeira API");
 });
 
 app.post('/usuario', async (req, res) =>{
-    const usuario  = req.body.nome
+    const nome  = req.body.nome;
+    const email = req.body.email;
+    const telefone = req.body.telefone;
+
+
     try {
         const docRef = await firestore.addDoc(firestore.collection(db, 'usuario'),{
             nome: nome,
+            email: email,
+            telefone: telefone
         })
-        res.send(docRef.id)
+        res.send("Usuário adicionado com sucesso:" + docRef.id)
     } catch (e) {
         console.log(e) 
 
